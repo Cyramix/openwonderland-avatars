7b319a816ddce67af098c59719365147cfa92d4b
rdahlgren <rdahlgren@ca5ce1e4-37eb-4c7f-a96f-f39c24e5598e>
2008-11-03 15:57:02 +0000

    Updating commenting.

--- src/imi/gui/JPanel_FileIO.java
+++ src/imi/gui/JPanel_FileIO.java
@@ -1,9 +1,20 @@
-/*
- * JPanel_FileIO.java
+/**
+ * Project Wonderland
  *
- * Created on September 22, 2008, 9:59 AM
+ * Copyright (c) 2004-2008, Sun Microsystems, Inc., All Rights Reserved
+ *
+ * Redistributions in source code form must reproduce the above
+ * copyright and this condition.
+ *
+ * The contents of this file are subject to the GNU General Public
+ * License, Version 2 (the "License"); you may not use this file
+ * except in compliance with the License. A copy of the License is
+ * available at http://www.opensource.org/licenses/gpl-license.php.
+ *
+ * $Revision$
+ * $Date$
+ * $State$
  */
-
 package imi.gui;
 
 import imi.scene.polygonmodel.PPolygonMeshInstance;
