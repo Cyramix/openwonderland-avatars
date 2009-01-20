f784daf2b8e76a8b11fa77c42dd5d1ea6dadd5d7
Gatts79 <Gatts79@ca5ce1e4-37eb-4c7f-a96f-f39c24e5598e>
2009-01-20 05:34:07 +0000

    visual changes per shawn

--- src/imi/character/networking/JNagClientGUI2.java
+++ src/imi/character/networking/JNagClientGUI2.java
@@ -134,6 +134,9 @@ public class JNagClientGUI2 extends javax.swing.JFrame implements ActionListener
         java.awt.GridBagConstraints gridBagConstraints;
 
         jSplitPane_MainWindow = new javax.swing.JSplitPane();
+        jSplitPane1 = new javax.swing.JSplitPane();
+        jPanel1 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
         jPanel_Main = new javax.swing.JPanel();
         jLabel_ConnectivityStatus = new javax.swing.JLabel();
         jPanel_InputPane = new javax.swing.JPanel();
@@ -149,15 +152,38 @@ public class JNagClientGUI2 extends javax.swing.JFrame implements ActionListener
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         getContentPane().setLayout(new java.awt.GridBagLayout());
 
-        jSplitPane_MainWindow.setDividerLocation(350);
+        jSplitPane_MainWindow.setDividerLocation(340);
         jSplitPane_MainWindow.setDividerSize(10);
         jSplitPane_MainWindow.setOrientation(javax.swing.JSplitPane.VERTICAL_SPLIT);
         jSplitPane_MainWindow.setMinimumSize(new java.awt.Dimension(640, 27));
         jSplitPane_MainWindow.setOneTouchExpandable(true);
         jSplitPane_MainWindow.setPreferredSize(new java.awt.Dimension(640, 500));
 
+        jSplitPane1.setDividerLocation(450);
+        jSplitPane1.setOneTouchExpandable(true);
+
+        jLabel1.setIcon(new javax.swing.ImageIcon("/Users/ptruong/Documents/Work/DarkStar/assets/textures/logos.png")); // NOI18N
+        jLabel1.setText("jLabel1");
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 175, Short.MAX_VALUE)
+            .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 334, Short.MAX_VALUE)
+            .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 334, Short.MAX_VALUE))
+        );
+
+        jSplitPane1.setRightComponent(jPanel1);
+
         jPanel_Main.setMinimumSize(new java.awt.Dimension(380, 0));
-        jPanel_Main.setPreferredSize(new java.awt.Dimension(640, 350));
+        jPanel_Main.setPreferredSize(new java.awt.Dimension(400, 325));
         jPanel_Main.setLayout(new java.awt.GridBagLayout());
         jPanel_Main.setFocusable(false);
 
@@ -186,7 +212,7 @@ public class JNagClientGUI2 extends javax.swing.JFrame implements ActionListener
         jComboBox_Selector.setPreferredSize(new java.awt.Dimension(140, 27));
         jPanel_InputPane.add(jComboBox_Selector, new java.awt.GridBagConstraints());
 
-        jTextField_InputWindow.setBackground(new java.awt.Color(153, 153, 255));
+        jTextField_InputWindow.setBackground(new java.awt.Color(255, 255, 204));
         jTextField_InputWindow.setPreferredSize(new java.awt.Dimension(300, 28));
         jTextField_InputWindow.setFocusable(true);
         jTextField_InputWindow.addActionListener(this);
@@ -208,7 +234,7 @@ public class JNagClientGUI2 extends javax.swing.JFrame implements ActionListener
 
         jScrollPane2.setAutoscrolls(true);
 
-        jTextArea_OutputWindow.setBackground(new java.awt.Color(153, 153, 255));
+        jTextArea_OutputWindow.setBackground(new java.awt.Color(255, 255, 204));
         jTextArea_OutputWindow.setColumns(20);
         jTextArea_OutputWindow.setRows(5);
         jScrollPane2.setViewportView(jTextArea_OutputWindow);
@@ -222,12 +248,14 @@ public class JNagClientGUI2 extends javax.swing.JFrame implements ActionListener
         gridBagConstraints.weighty = 1.0;
         jPanel_Main.add(jScrollPane2, gridBagConstraints);
 
-        jSplitPane_MainWindow.setLeftComponent(jPanel_Main);
+        jSplitPane1.setLeftComponent(jPanel_Main);
+
+        jSplitPane_MainWindow.setLeftComponent(jSplitPane1);
 
         jScrollPane1.setAutoscrolls(true);
         jScrollPane1.setPreferredSize(new java.awt.Dimension(640, 200));
 
-        jTable_Boards.setBackground(new java.awt.Color(153, 153, 255));
+        jTable_Boards.setBackground(new java.awt.Color(255, 255, 204));
         jTable_Boards.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
 
@@ -259,11 +287,14 @@ public class JNagClientGUI2 extends javax.swing.JFrame implements ActionListener
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JComboBox jComboBox_Selector;
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel_ConnectivityStatus;
+    private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel_InputPane;
     private javax.swing.JPanel jPanel_Main;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JSplitPane jSplitPane1;
     private javax.swing.JSplitPane jSplitPane_MainWindow;
     private javax.swing.JTable jTable_Boards;
     private javax.swing.JTextArea jTextArea_OutputWindow;
