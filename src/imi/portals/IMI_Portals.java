0caae75babee445691a2b644098abe914c499c31
Gatts79 <Gatts79@ca5ce1e4-37eb-4c7f-a96f-f39c24e5598e>
2009-03-24 00:33:28 +0000

    tweaks to portal scaling

--- src/imi/portals/IMI_Portals.java
+++ src/imi/portals/IMI_Portals.java
@@ -99,6 +99,8 @@ public class IMI_Portals extends Entity {
         portal.setLocalScale(transform.getScaleVector());
         portal.setRenderState(cs);
         portal.setModelBound(new BoundingBox());
+        portal.updateModelBound();
+        portal.updateWorldBound();
         portalObject.attachChild(portal);
 
         TextureState ts = wm.getRenderManager().createTextureState();
@@ -123,11 +125,11 @@ public class IMI_Portals extends Entity {
 
         center      = transform.getTranslation().add(new Vector3f(0.0f, (portalDimensions.y/1.9f), 1.3f * offset));
 //        portalFrame = new Box("topSide", center, 2.9f, 0.3f, 2.0f);
-        portalFrame = new Box("topSide", center, portalDimensions.x/1.95f, portalDimensions.z, portalDimensions.y/1.95f);
+        portalFrame = new Box("topSide", center, portalDimensions.x/1.9f, portalDimensions.z, portalDimensions.y/2.2f);
         portalFrame.setRenderState(ts);
         portalObject.attachChild(portalFrame);
 
-        center      = transform.getTranslation().add(new Vector3f(0.0f, 0.0f, 3.0f * offset));
+        center      = transform.getTranslation().add(new Vector3f(0.0f, 0.0f, 3.4f * offset));
 //        portalFrame = new Box("backSide", center, 2.3f, 2.0f, 0.3f);
         portalFrame = new Box("backSide", center, portalDimensions.x/2, portalDimensions.y/2, portalDimensions.z/2);
         portalFrame.setRenderState(ts);
