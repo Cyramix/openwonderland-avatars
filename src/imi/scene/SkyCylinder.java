b099b8c837831f9c10f95a32992139f404aeaa2f
rdahlgren <rdahlgren@ca5ce1e4-37eb-4c7f-a96f-f39c24e5598e>
2009-05-25 20:50:49 +0000

    SkyCylinder complete.

--- src/imi/scene/SkyCylinder.java
+++ src/imi/scene/SkyCylinder.java
@@ -22,7 +22,9 @@ import com.jme.image.Texture;
 import com.jme.math.Quaternion;
 import com.jme.renderer.Renderer;
 import com.jme.scene.Node;
+import com.jme.scene.TriMesh;
 import com.jme.scene.shape.Cylinder;
+import com.jme.scene.shape.Disk;
 import com.jme.scene.state.RenderState.StateType;
 import com.jme.scene.state.TextureState;
 import com.jme.scene.state.ZBufferState;
@@ -35,10 +37,18 @@ import org.jdesktop.mtgame.WorldManager;
  */
 public class SkyCylinder extends Node
 {
+    public enum SkyParts {
+        Cylinder,
+        TopDisk,
+        BottomDisk,
+    }
     /** The World Manager for renderstate creation. */
     private WorldManager m_wm = null;
 
-    private Cylinder skyCylinder = null;
+    /** Geometry for the sky **/
+    private Cylinder skyCylinder    = null;
+    private Disk     skyTop         = null;
+    private Disk     skyBottom      = null;
 
      /**
      * Creates a new skybox. The size of the skybox and name is specified here.
@@ -62,60 +72,98 @@ public class SkyCylinder extends Node
     /**
      * Set the texture for the sky cylinder. Should be a panoramic projection
      * @param texture The texture to use
+     * @param destinationSection Where does this texture belong?
      */
-    public void setTexture(Texture texture) {
-        TextureState ts = (TextureState) skyCylinder.getRenderState(StateType.Texture);
+    public void setTexture(Texture texture, SkyParts destinationSection) {
+        TextureState ts = null;
+        TriMesh target = null;
+        switch (destinationSection)
+        {
+            case BottomDisk:
+                ts = (TextureState) skyBottom.getRenderState(StateType.Texture);
+                target = skyBottom;
+                break;
+            case Cylinder:
+                ts = (TextureState) skyCylinder.getRenderState(StateType.Texture);
+                target = skyCylinder;
+                break;
+            case TopDisk:
+                ts = (TextureState) skyTop.getRenderState(StateType.Texture);
+                target = skyTop;
+                break;
+        }
+
         if (ts == null)
             ts = (TextureState)m_wm.getRenderManager().createRendererState(StateType.Texture);
 
         // Initialize the texture state
         ts.setTexture(texture, 0);
         ts.setEnabled(true);
-        skyCylinder.setRenderState(ts);
+        target.setRenderState(ts);
         // get an update
-        m_wm.addToUpdateList(skyCylinder);
+        m_wm.addToUpdateList(target);
     }
 
 
     private void initialize() {
 
         // make the sky geometry
+        // body
         skyCylinder = new Cylinder("Sky", 32, 16, 5,5, false, true);
         skyCylinder.setLocalRotation(new Quaternion(new float[] {3.14159f * -0.5f, 0, 0}));
+        // top
+        skyTop = new Disk("SkyTop", 8, 16, 5);
+        skyTop.setLocalRotation(new Quaternion(new float[] {3.14159f * 0.5f, 0, 0}));
+        skyTop.setLocalTranslation(0, 2.5f, 0);
+        // bottom
+        skyBottom = new Disk("SkyTop", 8, 16, 5);
+        skyBottom.setLocalRotation(new Quaternion(new float[] {3.14159f * -0.5f, 0, 0}));
+        skyBottom.setLocalTranslation(0, -2.5f, 0);
 
+        setRenderStates(skyTop);
+        setRenderStates(skyBottom);
+        setRenderStates(skyCylinder);
+
+        // And attach the skybox as a child
+        attachChild(skyCylinder);
+        attachChild(skyTop);
+        attachChild(skyBottom);
+        // get an update
+        m_wm.addToUpdateList(skyCylinder);
+        m_wm.addToUpdateList(skyBottom);
+        m_wm.addToUpdateList(skyTop);
+        m_wm.addToUpdateList(this);
+    }
+
+    private void setRenderStates(TriMesh target)
+    {
         // We don't want the light to effect our skybox
-        skyCylinder.setLightCombineMode(LightCombineMode.Off);
-        skyCylinder.setTextureCombineMode(TextureCombineMode.Replace);
+        target.setLightCombineMode(LightCombineMode.Off);
+        target.setTextureCombineMode(TextureCombineMode.Replace);
 
         ZBufferState zbuff = (ZBufferState)m_wm.getRenderManager().createRendererState(StateType.ZBuffer);
         zbuff.setWritable(false);
         zbuff.setEnabled(true);
         zbuff.setFunction(ZBufferState.TestFunction.LessThanOrEqualTo);
-        skyCylinder.setRenderState(zbuff);
+        target.setRenderState(zbuff);
 
         // We don't want it making our skybox disapear, so force view
-        skyCylinder.setCullHint(CullHint.Never);
+        target.setCullHint(CullHint.Never);
 
         // Make sure texture is only what is set.
-        skyCylinder.setTextureCombineMode(TextureCombineMode.Replace);
+        target.setTextureCombineMode(TextureCombineMode.Replace);
 
         // Make sure no lighting on the skybox
-        skyCylinder.setLightCombineMode(LightCombineMode.Off);
+        target.setLightCombineMode(LightCombineMode.Off);
 
         // Make sure the quad is viewable
-        skyCylinder.setCullHint(CullHint.Never);
+        target.setCullHint(CullHint.Never);
 
         // Set a bounding volume
-        skyCylinder.setModelBound(new BoundingBox());
-        skyCylinder.updateModelBound();
+        target.setModelBound(new BoundingBox());
+        target.updateModelBound();
 
-        skyCylinder.setRenderQueueMode(Renderer.QUEUE_SKIP);
-        skyCylinder.setVBOInfo(null);
-
-        // And attach the skybox as a child
-        attachChild(skyCylinder);
-        // get an update
-        m_wm.addToUpdateList(skyCylinder);
-        m_wm.addToUpdateList(this);
+        target.setRenderQueueMode(Renderer.QUEUE_SKIP);
+        target.setVBOInfo(null);
     }
 }
